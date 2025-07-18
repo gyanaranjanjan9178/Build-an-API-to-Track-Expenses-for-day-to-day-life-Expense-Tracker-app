ce8bdb9073edc5e4d9f53e0c4837d9b3eb0cf46e
gyanaranjanjan9178 <158822201+gyanaranjanjan9178@users.noreply.github.com>
2025-07-19 04:09:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
 sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
